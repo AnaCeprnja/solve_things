bee6bcdbf2d789a8566f79b7ad669ebe65325896
Ana Ceprnja <aceprnja@iun.edu>
2021-04-08 11:48:21 -0600

    done

--- solve.py
+++ solve.py
@@ -1,16 +1,33 @@
-import collections
-import math
-import os
-import random
-import re
-import sys
-
-
+def leftRotateByOne(A):
+ 
+    first = A[0]
+    for i in range(len(A) - 1):
+        A[i] = A[i + 1]
+ 
+    A[-1] = first
+ 
+ 
+# Function to left-rotate a list by `r` positions
+def leftRotate(A, r):
+ 
+    for i in range(r):
+        leftRotateByOne(A)
+ 
+ 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    n,k=map(int, input().split())
-    arr = [int(i) for i in input().split()]
-    d=collections.deque(arr)
-    d.rotate(-k)
-    fin = list(d)
-    print(' '.join(str(i) for i in d))
+
+    A = [1, 2, 3, 4, 5] #my  array
+    r = 4 # the amount of times it will rotate 
+ 
+    leftRotate(A, r)
+    print(A)
+
+    ##########
+def array_left_rotation(a, n, k):
+    a = list(a)
+    return a[k:] + a[:k]
+
+n, k = map(int, input().strip().split(' '))
+a = map(int, input().strip().split(' '))
+answer = array_left_rotation(a, n, k);
+print(*answer, sep=' ')
