cae6d34bc3ce078ef6f399521eac9e8e77cf62e2
Ana Ceprnja <aceprnja@iun.edu>
2021-04-08 11:48:39 -0600

    p

--- solve.py
+++ solve.py
@@ -23,11 +23,3 @@ if __name__ == '__main__':
     print(A)
 
     ##########
-def array_left_rotation(a, n, k):
-    a = list(a)
-    return a[k:] + a[:k]
-
-n, k = map(int, input().strip().split(' '))
-a = map(int, input().strip().split(' '))
-answer = array_left_rotation(a, n, k);
-print(*answer, sep=' ')
